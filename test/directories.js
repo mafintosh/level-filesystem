01630afc2b90fe0ec06942a3a749727fe0cf5b85
Mathias Buus <mathiasbuus@gmail.com>
2014-03-29 18:37:04 +0100

    readdir test and cleanup

--- test/directories.js
+++ test/directories.js
@@ -10,8 +10,6 @@ var reset = function() {
 test('mkdir', function(t) {
 	var fs = reset();
 
-	t.plan(6);
-
 	fs.mkdir('/foo/bar', function(err) {
 		t.ok(err);
 		t.same(err.code, 'ENOENT');
@@ -25,6 +23,7 @@ test('mkdir', function(t) {
 
 				fs.mkdir('/foo/bar', function(err) {
 					t.notOk(err);
+					t.end();
 				});
 			});
 		});
@@ -34,27 +33,25 @@ test('mkdir', function(t) {
 test('mkdir + stat', function(t) {
 	var fs = reset();
 
-	t.plan(3);
-
 	fs.mkdir('/foo', function() {
 		fs.stat('/foo', function(err, stat) {
 			t.notOk(err);
 			t.same(stat.mode, 0777);
 			t.ok(stat.isDirectory());
+			t.end();
 		});
 	});
 });
 
-test('mkdir with mode + stat', function(t) {
+test('mkdir with modes', function(t) {
 	var fs = reset();
-
-	t.plan(3);
 
 	fs.mkdir('/foo', 0766, function() {
 		fs.stat('/foo', function(err, stat) {
 			t.notOk(err);
 			t.same(stat.mode, 0766);
 			t.ok(stat.isDirectory());
+			t.end();
 		});
 	});
 });
@@ -62,10 +59,8 @@ test('mkdir with mode + stat', function(t) {
 test('rmdir', function(t) {
 	var fs = reset();
 
-	t.plan(5);
-
 	fs.rmdir('/', function(err) {
-		t.ok(true);
+		t.ok(err);
 		t.same(err.code, 'EPERM');
 
 		fs.mkdir('/foo', function() {
@@ -74,6 +69,55 @@ test('rmdir', function(t) {
 				fs.rmdir('/foo', function(err) {
 					t.ok(err);
 					t.same(err.code, 'ENOENT');
+					t.end();
+				});
+			});
+		});
+	});
+});
+
+test('readdir', function(t) {
+	var fs = reset();
+
+	fs.readdir('/', function(err, list) {
+		t.notOk(err);
+		t.same(list, []);
+
+		fs.readdir('/foo', function(err, list) {
+			t.ok(err);
+			t.notOk(list);
+			t.same(err.code, 'ENOENT');
+
+			fs.mkdir('/foo', function() {
+				fs.readdir('/', function(err, list) {
+					t.notOk(err);
+					t.same(list, ['foo']);
+
+					fs.readdir('/foo', function(err, list) {
+						t.notOk(err);
+						t.same(list, []);
+						t.end();
+					});
+				});
+			});
+		});
+	});
+});
+
+test('readdir not recursive', function(t) {
+	var fs = reset();
+
+	fs.mkdir('/foo', function() {
+		fs.mkdir('/foo/bar', function() {
+			fs.mkdir('/foo/bar/baz', function() {
+				fs.readdir('/foo', function(err, list) {
+					t.notOk(err);
+					t.same(list, ['bar']);
+					fs.readdir('/foo/bar', function(err, list) {
+						t.notOk(err);
+						t.same(list, ['baz']);
+						t.end();
+					});
 				});
 			});
 		});
